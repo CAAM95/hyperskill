8dfe9bbc2500ee367224fb3fdf4239a65cf89d36
Chris Ascencio <ascencio.chris1@gmail.com>
2026-02-13 16:24:33 -0600

    Update Web Scraper.py

--- Web Scraper.py
+++ Web Scraper.py
@@ -14,7 +14,7 @@ def get_base_url(url):
     base_url = f"{parsed.scheme}://{parsed.netloc}"
     return base_url
 
-def get_articles(soup, input_type):
+def get_articles(soup, base_url, input_type):
     articles = soup.find_all('article')
     data = {}
     i = 0
@@ -32,13 +32,10 @@ def get_articles(soup, input_type):
 def find_content_for_articles(articles):
     for i, article in enumerate(articles):
         soup = get_the_soup(articles[i]["url"])
-
         title = find_article_title(soup)
         articles[i]["title"] = title
-
         content = find_article_content(soup)
         articles[i]["content"] = content
-
     return articles
 
 def find_article_title(soup):
@@ -55,31 +52,32 @@ def create_files(articles):
         title = articles[i]['title']
         title = title.translate(str.maketrans("", "", string.punctuation))
         title = title.replace(" ", "_")
-
         with open(f"{title}.txt", 'wb') as file:
             file.write(articles[i]["content"].encode('utf-8'))
 
+def find_articles_for_page_n_times(number_of_pages, url, article_type):
+    soup = get_the_soup(url)
+    base_url = get_base_url(url)
+    next_page = None
+    for i in range(int(number_of_pages)):
+        if next_page:
+            url = urljoin(base_url, next_page.get('href', ''))
+            soup = get_the_soup(url)
+            base_url = get_base_url(url)
 
+        articles = get_articles(soup, base_url, article_type)
+        articles = find_content_for_articles(articles)
+        os.makedirs(f"Page_{i + 1}", exist_ok=True)
+        os.chdir(f"Page_{i + 1}")
+        create_files(articles)
+        os.chdir('..')
+        next_page = soup.find('a', rel='next') or soup.find('a', {'class': 'c-pagination__link'})
 
-number_of_pages = input()
-url = "https://www.nature.com/nature/articles?year=2020"
-article_type = input()
+def main():
+    number_of_pages = input()
+    url = "https://www.nature.com/nature/articles?year=2020"
+    article_type = input()
+    find_articles_for_page_n_times(number_of_pages, url, article_type)
 
-soup = get_the_soup(url)
-base_url = get_base_url(url)
-next_page = None
-for i in range(int(number_of_pages)):
-    if next_page:
-        url = urljoin(base_url, next_page.get('href', ''))
-        soup = get_the_soup(url)
-        base_url = get_base_url(url)
-
-    articles = get_articles(soup, article_type)
-    articles = find_content_for_articles(articles)
-    os.makedirs(f"Page_{i+1}", exist_ok=True)
-    os.chdir(f"Page_{i+1}")
-    create_files(articles)
-    os.chdir('..')
-    next_page = soup.find('a', rel='next') or soup.find('a', {'class': 'c-pagination__link'})
-
-
+if __name__ == "__main__":
+    main()
